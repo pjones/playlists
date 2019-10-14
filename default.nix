bf358fd014efdd4e22e34d0408230cd2639ba5c6
Peter Jones <pjones@devalot.com>
2019-10-14 08:15:59 -0700

    Update dependency: optparse-applicative

--- default.nix
+++ default.nix
@@ -4,7 +4,7 @@
 let
   nix-hs-src = fetchGit {
     url = "https://code.devalot.com/open/nix-hs.git";
-    rev = "2003332a1e8e518b54e6143f9a9467a8a05abca4";
+    rev = "a2b666faf8cb3c6f769655dfb36f4695f78bc3c3";
   };
 
   nix-hs = import "${nix-hs-src}/default.nix" { inherit pkgs; };
@@ -12,4 +12,11 @@ let
 in nix-hs {
   cabal = ./playlists.cabal;
   flags = [ "maintainer" "test-doctest" ];
+
+  overrides = lib: self: super: with lib; {
+    optparse-applicative =
+      if super ? optparse-applicative_0_15_0_0
+        then super.optparse-applicative_0_15_0_0
+        else optparse-applicative;
+  };
 }
