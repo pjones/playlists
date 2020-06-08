c615bf863aa179c28acd46005fdc9e4afb15b393
Peter Jones <pjones@devalot.com>
2020-06-08 12:44:43 -0700

    Upgrade nix-hs

--- default.nix
+++ default.nix
@@ -1,22 +1,8 @@
-{ pkgs ? import <nixpkgs> { }
-}:
+{ sources ? import ./nix/sources.nix, pkgs ? import sources.nixpkgs { }
+, nix-hs ? import sources.nix-hs { inherit pkgs; }, compiler ? "default" }:
 
-let
-  nix-hs-src = fetchGit {
-    url = "https://code.devalot.com/open/nix-hs.git";
-    rev = "a2b666faf8cb3c6f769655dfb36f4695f78bc3c3";
-  };
-
-  nix-hs = import "${nix-hs-src}/default.nix" { inherit pkgs; };
-
-in nix-hs {
+nix-hs {
+  inherit compiler;
   cabal = ./playlists.cabal;
   flags = [ "maintainer" "test-doctest" ];
-
-  overrides = lib: self: super: with lib; {
-    optparse-applicative =
-      if super ? optparse-applicative_0_15_0_0
-        then super.optparse-applicative_0_15_0_0
-        else optparse-applicative;
-  };
 }
