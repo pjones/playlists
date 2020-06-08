bed0d40af717b4454ef639273ae6679a34e7c665
Peter Jones <pjones@devalot.com>
2020-06-08 13:25:43 -0700

    Revert compiler attribute

--- default.nix
+++ default.nix
@@ -1,8 +1,8 @@
 { sources ? import ./nix/sources.nix, pkgs ? import sources.nixpkgs { }
-, nix-hs ? import sources.nix-hs { inherit pkgs; }, compiler ? "default" }:
+, nix-hs ? import sources.nix-hs { inherit pkgs; }, ghc ? "default" }:
 
 nix-hs {
-  inherit compiler;
   cabal = ./playlists.cabal;
+  compiler = ghc;
   flags = [ "maintainer" "test-doctest" ];
 }
