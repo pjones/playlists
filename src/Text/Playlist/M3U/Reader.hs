7a2b5c8c5e6fd82b726bf445b61d333cc8abb965
Nikolai Kudasov <nickolay.kudasov@gmail.com>
2020-07-30 23:25:19 +0300

    Add comments and global tags

--- src/Text/Playlist/M3U/Reader.hs
+++ src/Text/Playlist/M3U/Reader.hs
@@ -19,11 +19,12 @@ module Text.Playlist.M3U.Reader where
 --------------------------------------------------------------------------------
 
 import           Control.Applicative
-import           Control.Monad                     (void)
+import           Control.Monad                     (unless)
 import           Data.Attoparsec.ByteString        (Parser)
 import qualified Data.Attoparsec.ByteString        as Atto
 import qualified Data.Attoparsec.ByteString.Char8  as Atto8
 import           Data.Char                         (ord)
+import           Data.Maybe                        (catMaybes)
 import           Data.Text                         (Text)
 import qualified Data.Text                         as Text
 import           Data.Text.Encoding                (decodeUtf8)
@@ -35,51 +36,62 @@ parsePlaylist = playlistParser
 
 playlistParser :: Parser Playlist
 playlistParser = do
-  playlistGlobalTags <- globalTagsParser
+  "#EXTM3U" *> Atto8.skipSpace
+  playlistGlobalTags <- anyTags
   playlistTracks <- many trackParser
   return Playlist{..}
 
-globalTagsParser :: Parser [Tag]
-globalTagsParser = do
-  "#EXTM3U" *> Atto8.skipSpace
-  skipComments
-  -- TODO: collect MEDIA-SEQUENCE and other global
-  return []
+tags :: (Tag -> Bool) -> Parser [Tag]
+tags p = catMaybes <$> many (tagOrComment p)
 
-skipComments :: Parser ()
-skipComments = return () -- void (many comment)
+anyTags :: Parser [Tag]
+anyTags = tags (const True)
 
-comment :: Parser ()
-comment = "#" *> notEXT
+globalTags :: Parser [Tag]
+globalTags = tags isGlobal
   where
-    notEXT = ("EXT" *> fail "")
-         <|> (Atto.takeTill isEOL *> Atto8.skipSpace)
+    isGlobal Tag{..} = tagName `elem`
+      [ "#EXTM3U"
+      , "#EXT-X-VERSION"
+      , "#EXT-X-TARGETDURATION"
+      , "#EXT-X-MEDIA-SEQUENCE"
+      ]
 
-tagParser :: Parser Tag
-tagParser = skipComments *> do
-  tagStr <- decodeUtf8 <$> ("#EXT" *> Atto.takeTill isEOL)
+tagOrComment :: (Tag -> Bool) -> Parser (Maybe Tag)
+tagOrComment p = "#" *> (("EXT" *> fmap Just (tagParser p)) <|> comment)
+  where
+    comment = Atto.takeTill isEOL *> Atto8.skipSpace *> pure Nothing
+
+tagParser :: (Tag -> Bool) -> Parser Tag
+tagParser p = do
+  -- #EXT
+  tagStr <- decodeUtf8 <$> Atto.takeTill isEOL
   let (name, val) = Text.break (== ':') tagStr
       tagName = TagName ("#EXT" <> name)
       tagValue = Text.drop 1 val
+      tag = Tag{..}
   Atto8.skipSpace
-  return Tag{..}
-  where
-    ord8 = fromIntegral . ord
+  unless (p tag) $ fail "Tag does not satisfy the predicate"
+  return tag
 
 trackParser :: Parser Track
 trackParser = do
-  trackTags <- many tagParser
+  trackTags <- anyTags
 --   case lookupTag "#EXTINF" trackTags of
 --     Tag { tagValue = durationAndTitle } -> do
 --       return
 --
 --       -- TODO: parse durationAndTitle
   trackURL <- parseURL
+  let trackTitle = Nothing
+      trackDuration = Nothing
   return Track{..}
 
 --------------------------------------------------------------------------------
 -- | Parser for URL or file name in a M3U file.  The URL is the entire
 -- line so this parser extracts the entire line and decodes it.
+--
+-- IMPORTANT: parseURL does not accept an empty line
 parseURL :: Parser Text
 parseURL = Text.pack <$> Atto8.many1 (Atto8.satisfy (not . Atto8.isEndOfLine . fromIntegral . ord)) <* Atto8.skipSpace
 
